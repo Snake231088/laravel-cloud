c82d349b59c2cbaa55131c81e621bf31539e0052
Diego Verì <diego.veri@weconstudio.it>
2025-02-24 17:24:36 +0100

    feat: laravel cloud

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('user', function () {
+    \App\Models\User::factory(10)->create();
+    
+    return response()->json(status: 204);
+});
